90fd2e514cbc2ce7128408c9debacf0812cde80e
CamilleHunot <camille.hunot@gmail.com>
2026-09-24 14:40:17 -0400

    add function to read json info

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -9,32 +9,59 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt #permet de configurer le comportement de facon précise des widgets pyside6
 #ex : necessaire pour trier les colonnes.
 
+from pathlib import Path #permet de récupérer le nom du fichier en se basant sur le path et autres (tailles etc)
+
+
+app = QApplication([]) #ici cela doit venir avant les widgets
 
 json_path = input("Donner le path du fichier Json") #mettre le lienpath
 
+def open_json():
 
+    #>>>>>>>> permet d'ouvrir et de load le .json en toute sécurité
+    try: 
+        file = open(json_path, encoding="utf-8") #ouvre le fichier 
+    # encoding="utf-8" sert à régler le problème des accents
 
-#>>>>>>>> permet d'ouvrir et de load le .json en toute sécurité
-try: 
-    file = open(json_path, encoding="utf-8") #ouvre le fichier 
-# encoding="utf-8" sert à régler le problème des accents
+        data_load = json.load(file) #met le fichier dans une variable
 
-    data = json.load(file) #met le fichier dans une variable
+    except:
+        print(f"Could not")
 
-except:
-    print(f"Could not")
+    #print(data) #vérifier si le fichier est bien load dans la console
 
-#print(data) #vérifier si le fichier est bien load dans la console
+    return data_load
+def info_json(json_path,data):
+    # récupérer le nom du fichier
+    path_fichier = Path(json_path)
+    nom_fichier = Path(path_fichier).name
 
+    #récupérer la taille du fichier
+    size_octets = path_fichier.stat().st_size # permet de stocker la taille du fichier en octe dans une variable
+    size_ko = round(size_octets / 1024, 2)
+    #round permet d'arrondir la valeur, on divise par 1024 car il y a cette quantité d'octet dans un ko, le ,2 sert a indiquer le nombre après la virgule
 
+    #nombre d'élément en try pour éviter les crashs
+    try:
+        element_number = len(data)
+
+    except:  
+
+        element_number = "error in element_number"  
+
+    return (nom_fichier, size_ko, element_number)
+
+data = open_json() #mise en variable du load  json
+
+name, size, element = info_json(json_path,data) #initialise des variable en groupe car elles résultent d'un return de la même fonction
+print(name)
+print(size)
+print(element)
 
 # >>>>>>>>> ici on va mettre en forme les bases du tableau, nbr de colonne et de ligne etc <<<<<<<
-app = QApplication([]) #ici cela doit venir avant les widgets
-
 
 headers = list(data[0].keys()) #permet de récupérer les headers en ce basant sur la première liste(position 0), leur quantité et leur nom
 #attention si data[1] ne possède pas le même nombre de key il faudrait trouver une facon de combinner les informations. 
-
 
 tableau = QTableWidget() #définition de notre tableau
 tableau.setRowCount(len(data)) #len data nous permet de vérifier combien de ligne en se basant sur la taille de data. Définit le nombre de ligne
@@ -46,6 +73,7 @@ tableau.sortByColumn(0, Qt.SortOrder.AscendingOrder) #trier en ordre croissant l
 
 
 #>>>>>>>>>> Mettre les values dans nos cases <<<<<<
+
 for row in range(len(data)):
     for column in range(len(headers)):
 
@@ -64,13 +92,9 @@ for row in range(len(data)):
             values = cell_values
 
 
-       # valuesTexte = (str(values)) #permet d'inscrire les chiffres en string
-        #attention nous allons devoir quand même faire une manipulation plus tard si l'on souhaite
-        #trié en ordre de chiffre
-
-        tableau.setItem(row, column, QTableWidgetItem(values))
-      #  tableau.setItem(row, len(headers), QTableWidgetItem(values))
+        tableau.setItem(row, column, QTableWidgetItem(values)) #permet de placé la value dans le tableau
         
+
 
 #>>>>> initialisation et fermeture de l'interface <<<<<<
 window = QMainWindow();
