09b7471f1a3b2deb1f3c50df025897465326003c
CamilleHunot <camille.hunot@gmail.com>
2026-09-24 16:36:41 -0400

    change name for .py

--- TP1/CamilleBerardHunot_TP1.py
+++ TP1/CamilleBerardHunot_TP1.py
@@ -12,15 +12,14 @@ from PySide6.QtWidgets import (
     QLineEdit,
     QPushButton,
 )
-from PySide6.QtCore import Qt #permet de configurer le comportement de facon précise des widgets pyside6
-#ex : necessaire pour trier les colonnes.
+from PySide6.QtCore import Qt #permet de configurer le comportement de facon précise des widgets pyside6, necessaire pour trier les colonnes.
 
 from pathlib import Path #permet de récupérer le nom du fichier en se basant sur le path et autres (tailles etc)
 
 
 app = QApplication([]) #ici cela doit venir avant les widgets
 
-json_path = input("Donner le path du fichier Json") #mettre le lienpath
+json_path = input("Donner le path du fichier Json") #mettre le lienpath 
 
 def open_json():
 
@@ -64,6 +63,18 @@ print(name)
 print(size)
 print(element)
 
+tableau = QTableWidget() #définition de notre tableau
+headers = list(data[0].keys()) #permet de récupérer les headers en ce basant sur la première liste(position 0), leur quantité et leur nom
+#attention si data[1] ne possède pas le même nombre de key il faudrait trouver une facon de combinner les informations. 
+
+tableau.setRowCount(len(data)) #len data nous permet de vérifier combien de ligne en se basant sur la taille de data. Définit le nombre de ligne
+tableau.setColumnCount(len(headers))#pour le moment fonctionne mais seulement si toute les ligne ont le meme nombre de key. Définit le nombre de colonne
+tableau.setHorizontalHeaderLabels(headers) #définit le titre des columns
+tableau.setSortingEnabled(True)#autorise a trier le tableau en fonction des colonnes
+tableau.sortByColumn(0, Qt.SortOrder.AscendingOrder) #trier en ordre croissant la première ligne de facon automatique
+
+
+
 #>>>>>>>>> mise en forme
 
 main_window = QWidget() #fenetre principale
@@ -80,35 +91,54 @@ titre.setStyleSheet("font-size : 16px; font-weight: bold;")
 main_box.addWidget(titre)#place le titre dans la main box
 
 
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>> recherche <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-#ajouter le tableau
-tableau = QTableWidget() #définition de notre tableau
+recherche_box = QHBoxLayout()
+recherche_ligne = QLineEdit()
+recherche_ligne.setPlaceholderText("Rechercher")
+recherche_box.addWidget(recherche_ligne) #ajouter la prise de texte dans notre boite de recherche
+main_box.addLayout(recherche_box) #ajouter notre boite de recherche dans notre main box
+
+def recherche(texte_cherche):
+    print("help")
+    texte_recherche = texte_cherche.lower() #permet d'ignorer les majuscules
+
+    for row in range(tableau.rowCount()):
+        texte_true = False #on commence par dire que la ligne ne correspond pas
+
+        for column in range(tableau.columnCount()):
+
+            # on sort la valeur pour la comparer, on doit refaire directement ici sinon on aura des problème avec la fonction text.changed
+            row_value = data[row]
+            key_variable = headers[column]
+            values = row_value.get(key_variable)
+
+
+            if values and texte_recherche in str(values).lower():
+                texte_true = True
+                break #si c'est vrai on sort de la boucle car on a une correspondance
+            else:
+                texte_true = False
+
+
+        if texte_true:
+            tableau.setRowHidden(row, False) #si c'est vrai on cache pas la ligne
+        else:
+            tableau.setRowHidden(row, True)   #si le texte ne correspond pas on cache la ligne      
+
+recherche_ligne.textChanged.connect(recherche) #permet de chercher a chaque changement de texte
+
+
+#>>>>>>>>>>>>>>>>>>>>>>> ajouter le tableau dans la main box <<<<<<<<<<<<<<<<<<<<<<
+
 main_box.addWidget(tableau)
 
-#ajouter autres infos
+#>>>>>>>>>>>>>>>>>>>>>>> ajouter autres infos <<<<<<<<<<<<<<<<<<<<<<
+
 texte_info = QLabel(f"Nom du fichier : {name}\ntaille : {size} ko \nQuantité : {element}")
 main_box.addWidget(texte_info)
 
-
-
-
-# >>>>>>>>> ici on va mettre en forme les bases du tableau, nbr de colonne et de ligne etc <<<<<<<
-
-headers = list(data[0].keys()) #permet de récupérer les headers en ce basant sur la première liste(position 0), leur quantité et leur nom
-#attention si data[1] ne possède pas le même nombre de key il faudrait trouver une facon de combinner les informations. 
-
-tableau.setRowCount(len(data)) #len data nous permet de vérifier combien de ligne en se basant sur la taille de data. Définit le nombre de ligne
-tableau.setColumnCount(len(headers))#pour le moment fonctionne mais seulement si toute les ligne ont le meme nombre de key. Définit le nombre de colonne
-tableau.setHorizontalHeaderLabels(headers) #définit le titre des columns
-tableau.setSortingEnabled(True)#autorise a trier le tableau en fonction des colonnes
-tableau.sortByColumn(0, Qt.SortOrder.AscendingOrder) #trier en ordre croissant la première ligne de facon automatique
-
-
-
-
-    
-
-#>>>>>>>>>> Mettre les values dans nos cases <<<<<<
+#>>>>>>>>>>>>>>>>>>>>>>> Mettre les values dans nos cases de <<<<<<<<<<<<<<<<<<<<<<
 
 for row in range(len(data)):
     for column in range(len(headers)):
@@ -127,56 +157,8 @@ for row in range(len(data)):
         else:
             values = cell_values
 
-
         tableau.setItem(row, column, QTableWidgetItem(values)) #permet de placé la value dans le tableau
         
-
-#>>>>> recherche
-#recherche
-recherche_box = QHBoxLayout()
-recherche_ligne = QLineEdit()
-recherche_ligne.setPlaceholderText("Rechercher")
-recherche_cta = QPushButton("go")
-
-
-recherche_box.addWidget(recherche_ligne)
-recherche_box.addWidget(recherche_cta)
-main_box.addLayout(recherche_box)
-
-def recherche(texte_cherche,values):
-    texte_recherche = texte_recherche.lower() #permet d'ignorer les majuscules
-    for row in range(len(data)):
-        texte_true = False #on commence par dire que la ligne ne correspond pas
-
-        for column in range(len(headers)):
-            if values and texte_recherche in values.text().lower():
-                texte_true = True
-                break
-        #if texte_true:
-          #  tableau.setRowHidden(row, False) #si c'est vrai on cache pas la ligne
-       # else:
-           # tableau.setRowHidden(row, True)   
-        tableau.setRowHidden(row, not texte_true)     
-            
-
-recherche_ligne.textChanged.connect(recherche)
-
-#>>>>> initialisation et fermeture de l'interface <<<<<<
-
-
-
-
-
-
-#vertical_box = QVBoxLayout()
-
-#widget.setLayout(vertical_box) #on place vertical box dans widget
-
-#window = QMainWindow()
-
-#window.setCentralWidget(tableau) #on ne voit pas le tableau sans cette ligne
-#vertical_box.addWidget(tableau)
-
-
+#>>>>> voir + fermeture de l'interface <<<<<<
 main_window.show()
 sys.exit(app.exec()) #important sinon la fenetre ne va jamais se fermer
